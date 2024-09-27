e9d71315056432f758f166c1dbce38b92a222fcc
Raghuvasagan-N <raghuvasagan1209@gmail.com>
2024-09-27 17:11:32 +0530

    inserted food class

--- VastRestaurant/src/main/java/com/vast/vo/Food.java
+++ VastRestaurant/src/main/java/com/vast/vo/Food.java
@@ -1,5 +1,70 @@
 package com.vast.vo;
 
 public class Food {
+	private int foodId;
+	private String foodName;
+	private double foodPrice;
+	private double foodRating;
+	private String foodType;
+
+	public Food() {
+		super();
+	}
+
+	public Food(int foodId, String foodName, double foodPrice, double foodRating, String foodType) {
+		super();
+		this.foodId = foodId;
+		this.foodName = foodName;
+		this.foodPrice = foodPrice;
+		this.foodRating = foodRating;
+		this.foodType = foodType;
+	}
+
+	public int getFoodId() {
+		return foodId;
+	}
+
+	public void setFoodId(int foodId) {
+		this.foodId = foodId;
+	}
+
+	public String getFoodName() {
+		return foodName;
+	}
+
+	public void setFoodName(String foodName) {
+		this.foodName = foodName;
+	}
+
+	public double getFoodPrice() {
+		return foodPrice;
+	}
+
+	public void setFoodPrice(double foodPrice) {
+		this.foodPrice = foodPrice;
+	}
+
+	public double getFoodRating() {
+		return foodRating;
+	}
+
+	public void setFoodRating(double foodRating) {
+		this.foodRating = foodRating;
+	}
+
+	public String getFoodType() {
+		return foodType;
+	}
+
+	public void setFoodType(String foodType) {
+		this.foodType = foodType;
+	}
+
+	@Override
+	public String toString() {
+		return "Food [foodId=" + foodId + ", foodName=" + foodName + ", foodPrice=" + foodPrice + ", foodRating="
+				+ foodRating + ", foodType=" + foodType + "]";
+	}
+	
 
 }
